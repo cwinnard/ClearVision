f03b3842bfb2ee63dc1d68bbb3a9f9f08ab41b31
Nate Britt <xmrbritt@gmail.com>
2015-11-11 15:22:14 -0500

    Added user controller

---
+++ ClearVision/src/com/clearvision/controller/MainController.java
@@ -0,0 +1,64 @@
+package com.clearvision.controller;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.clearvision.database.UserDB;
+import com.clearvision.model.User;
+
+
+/**
+ * Servlet implementation class MainController
+ */
+@WebServlet("/MainController")
+public class MainController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MainController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getParameter("action");
+		UserDB userDB = new UserDB();
+
+		switch (action) {
+		case "add": {
+
+			User user = new User();
+			user.setFirstName(request.getParameter("FirstName"));
+			user.setLastName(request.getParameter("LastName"));
+			user.setEmail(request.getParameter("Email"));
+			user.setPass(request.getParameter("Pass"));
+			userDB.addUserToDatabase(user);
+
+			break;
+
+		}
+		}
+	}
+}
