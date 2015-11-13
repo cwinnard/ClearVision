e7c41a97192067cc580be9437effeff48b1894e3
jtsjeremy <jts@loopctrl.com>
2015-11-13 12:01:10 -0500

    adding BookmarkDao.java and BkmarkDaoImpl.java files

---
+++ ClearVision/src/com/clearvision/database/BkmarkDaoImpl.java
@@ -0,0 +1,101 @@
+package com.clearvision.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.clearvision.model.Bookmark;
+
+public class BkmarkDaoImpl implements BookmarkDao {
+	int rowsChanged;
+	private String statementString;
+	
+	public BkmarkDaoImpl() {
+	}
+	
+	@Override
+	public void saveBookmarksToDB(List<Bookmark> bookmarks) {
+		 statementString = "Insert into Bookmarks (Name, URL, UserIDLink) Values (?,?,?)";
+		 
+		try {
+			DatabaseConnection connector = new DatabaseConnection();
+			Connection con = connector.connectToDB();
+			
+			PreparedStatement addBookmarksToBookmarkTable = con.prepareStatement(statementString);
+			for (Bookmark bookmark : bookmarks) {
+				addBookmarksToBookmarkTable.setString(1, bookmark.getName());
+				addBookmarksToBookmarkTable.setString(2, bookmark.getUrl());
+				addBookmarksToBookmarkTable.setString(3, "1");
+				rowsChanged = addBookmarksToBookmarkTable.executeUpdate();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void addBkMarkToDb(Bookmark bookmark) {
+		 statementString = "Insert into Bookmarks (Name, URL, UserIDLink) Values (?,?,?)";
+		
+		try {
+			DatabaseConnection connector = new DatabaseConnection();
+			Connection con = connector.connectToDB();
+
+			PreparedStatement addBookmarkToBookmarkTable = con.prepareStatement(statementString);
+			addBookmarkToBookmarkTable.setString(1, bookmark.getName());
+			addBookmarkToBookmarkTable.setString(2, bookmark.getUrl());
+			addBookmarkToBookmarkTable.setString(3, "1");
+			rowsChanged = addBookmarkToBookmarkTable.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void deleteBkMarkFromDb(Bookmark bookmark) {
+		 statementString = "Delete from Bookmarks where URL = ?";
+		
+		try {
+			DatabaseConnection connector = new DatabaseConnection();
+			Connection con = connector.connectToDB();
+			
+			PreparedStatement deleteBookmarkFromBookmarkTable = con.prepareStatement(statementString);
+			deleteBookmarkFromBookmarkTable.setString(1, bookmark.getUrl());
+			rowsChanged = deleteBookmarkFromBookmarkTable.executeUpdate();		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public Bookmark getBkMarkFromDb(String name) {
+	
+			Bookmark bookmarkInfo = new Bookmark();
+
+			statementString = "SELECT * FROM Bookmarks Where name = ?";
+			
+			try {
+				DatabaseConnection connector = new DatabaseConnection();
+				Connection con = connector.connectToDB();
+
+				PreparedStatement getBkmkFromBkmkTable = con.prepareStatement(statementString);
+				ResultSet bkmResults = getBkmkFromBkmkTable.executeQuery();
+				
+				String bkmkUrl = bkmResults.getString(1);
+				 String bkmkName = bkmResults.getString(2);
+				 		
+				rowsChanged = getBkmkFromBkmkTable.executeUpdate();
+				bookmarkInfo.setName(bkmkName);
+				bookmarkInfo.setUrl(bkmkUrl);
+			
+			}catch (SQLException e) {
+				e.printStackTrace();
+			} return bookmarkInfo;
+		}
+	@Override
+	public void updateBkMarkInDb(int userId) {
+	}	
+}
+
